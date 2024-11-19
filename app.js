ea0ada54a2c48758d72c3f660648dfe8ee84f00c
dhruvprajapati2610 <140337459+dhruvprajapati2610@users.noreply.github.com>
2024-11-19 21:21:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -247,6 +247,24 @@ app.get('/contact-us',(req,res)=>{
   res.render('contact-us')
 })
 
+app.get('/crpclist',(req,res)=>{
+  res.render('crpclist.ejs');
+})
+
+app.get('/crpc',async(req,res)=>{
+  const chapter = req.query.chapter;
+  try{
+    var sql = 'select * from crpc_data where chapter_number=$1'
+    var crpc = await pool.query(sql,[chapter]);
+    console.log(crpc.rows);
+    res.render('crpcDrop.ejs',{data: crpc.rows});
+  } catch (err) {
+    console.error('Error retrieving data from the database:',err);
+    res.status(500).send('Internal server error');
+  }
+})
+
+
 
 app.get('/',async(req,res)=>{
   try{
