1c31a830114ac48aa0cc7b41b621e630692a3b3d
GarvThanvi <gtuhm2004@gmail.com>
2024-12-26 13:36:14 +0530

    bns search implemented

--- app.js
+++ app.js
@@ -527,6 +527,85 @@ app.get('/bns_section', async (req, res) => {
   }
 });
 
+app.get('/bns_sections/search', async (req, res) => {
+  const searchTerm = req.query.search;
+  if (!searchTerm) {
+      return res.status(400).json({
+          success: false,
+          message: 'Search term is required',
+          data: [],
+      });
+    }
+  const query = `SELECT * FROM bns_sections WHERE section_name ILIKE $1 OR description ILIKE $1 OR section_number ILIKE $1 `;
+  try {
+      const { rows } = await pool.query(query, [`%${searchTerm}%`]);
+      res.json({
+          success: true,
+          message: rows.length > 0 ? 'Search results fetched successfully' : 'No results found',
+          data: rows,
+      });
+  } catch (err) {
+      console.error('Error executing search query:', err);
+      res.status(500).json({
+          success: false,
+          message: 'An error occurred while fetching search results',
+          data: [],
+      });
+  }
+});
+
+app.get('/bns_sections/search/:chapterNumber', async (req, res) => {
+  const searchTerm = req.query.search;
+  const chapterNumber = req.params.chapterNumber;
+  if (!searchTerm) {
+      return res.status(400).json({
+          success: false,
+          message: 'Search term is required',
+          data: [],
+      });
+    }
+  const query = `SELECT * FROM bns_sections WHERE (section_name ILIKE $1 OR description ILIKE $1 OR section_number ILIKE $1) 
+                 AND chapter_number = $2
+ `;
+  try {
+      const { rows } = await pool.query(query, [`%${searchTerm}%`, chapterNumber]);
+      res.json({
+          success: true,
+          message: rows.length > 0 ? 'Search results fetched successfully' : 'No results found',
+          data: rows,
+      });
+  } catch (err) {
+      console.error('Error executing search query:', err);
+      res.status(500).json({
+          success: false,
+          message: 'An error occurred while fetching search results',
+          data: [],
+      });
+  }
+});
+
+app.get('/bns_sections/:chapterNumber', async(req, res) => {
+  const chapterNumber = req.params.chapterNumber;
+  const query = `select * from bns_sections where chapter_number = $1`;
+
+  try {
+    const {rows} = await pool.query(query, [chapterNumber]);
+    res.json({
+      success: true,
+      message : "Bns sections fetched",
+      data: rows,
+  });
+  } catch (error) {
+    console.error('Error fetching bns_sections for a chapter', err);
+      res.status(500).json({
+          success: false,
+          message: 'An error occurred while fetching bns_sections results',
+          data: [],
+      });
+  }
+})
+
+
 app.get('/search-ipc',async(req,res)=>{
   try{
    const searchQuery = req.query.search||'';
