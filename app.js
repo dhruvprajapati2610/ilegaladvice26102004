f0ccb8c6da691b0543bff7bab2c956df57574727
GarvThanvi <gtuhm2004@gmail.com>
2025-01-24 16:50:22 +0530

    CPC SECTIONS

--- app.js
+++ app.js
@@ -965,6 +965,132 @@ app.get("/bnss_sections/search/:chapterNumber", async (req, res) => {
   }
 });
 
+//CPC SECTIONS
+app.get("/cpc", (req, res) => {
+  res.render("cpcChapters.ejs");
+});
+
+app.get("/cpc_sections", async (req, res) => {
+  const chapter = parseInt(req.query.chapter);
+  if (isNaN(chapter)) {
+    return res.status(400).send("Invalid chapter number");
+  }
+  const query = "SELECT * FROM cpc_sections WHERE chapter_number = $1 ORDER BY section_number";
+  try {
+    const { rows } = await pool.query(query, [chapter]);
+    res.render("cpcSectionlist.ejs", { data: rows });
+  } catch (err) {
+    console.error("Error executing query:", err);
+    res.status(500).send("Server error");
+  }
+});
+
+app.get("/cpc_section", async (req, res) => {
+  const section_number = req.query.section_number;
+  const query = "SELECT * FROM cpc_sections WHERE section_number = $1";
+
+  try {
+    const { rows } = await pool.query(query, [section_number]);
+    if (rows.length === 0) {
+      return res.status(404).send("Section not found");
+    }
+    res.render("cpcSection.ejs", { data: rows[0] });
+  } catch (err) {
+    console.error("Error executing query:", err);
+    res.status(500).send("Server error");
+  }
+});
+
+app.get("/cpc_sections/search", async (req, res) => {
+  const searchTerm = req.query.search;
+  if (!searchTerm) {
+    return res.status(400).json({
+      success: false,
+      message: "Search term is required",
+      data: [],
+    });
+  }
+  const query = `SELECT * FROM cpc_sections WHERE section_number ILIKE $1 OR section_name ILIKE $1 OR description ILIKE $1 `;
+  try {
+    const { rows } = await pool.query(query, [`%${searchTerm}%`]);
+    res.json({
+      success: true,
+      message:
+        rows.length > 0
+          ? "Search results fetched successfully"
+          : "No results found",
+      data: rows,
+    });
+  } catch (err) {
+    console.error("Error executing search query:", err);
+    res.status(500).json({
+      success: false,
+      message: "An error occurred while fetching search results",
+      data: [],
+    });
+  }
+}); 
+
+app.get("/cpc_sections/search/:chapterNumber", async (req, res) => {
+  const searchTerm = req.query.search;
+  const chapterNumber = req.params.chapterNumber;
+  if (!searchTerm && searchTerm === " ") {
+    return res.status(400).json({
+      success: false,
+      message: "Search term is required",
+      data: [],
+    });
+  }
+  const query = `SELECT * FROM cpc_sections WHERE (section_name ILIKE $1 OR section_number ILIKE $1) 
+                 AND chapter_number = $2
+                 `;
+  try {
+    const { rows } = await pool.query(query, [
+      `%${searchTerm}%`,
+      chapterNumber,
+    ]);
+    res.json({
+      success: true,
+      message:
+        rows.length > 0
+          ? "Search results fetched successfully"
+          : "No results found",
+      data: rows,
+    });
+  } catch (err) {
+    console.error("Error executing search query:", err);
+    res.status(500).json({
+      success: false,
+      message: "An error occurred while fetching search results",
+      data: [],
+    });
+  }
+});
+
+app.get("/cpc_sections/:chapterNumber", async (req, res) => {
+  const chapterNumber = req.params.chapterNumber;
+  const query = `select * from cpc_sections where chapter_number = $1`;
+
+  try {
+    const { rows } = await pool.query(query, [chapterNumber]);
+    res.json({
+      success: true,
+      message: "cpc sections fetched",
+      data: rows,
+    });
+  } catch (error) {
+    console.error("Error fetching cpc_sections for a chapter", err);
+    res.status(500).json({
+      success: false,
+      message: "An error occurred while fetching cpc_sections results",
+      data: [],
+    });
+  }
+});
+
+
+
+
 
 
 app.get("/userAccount", isuAuthenticated, async (req, res) => {
